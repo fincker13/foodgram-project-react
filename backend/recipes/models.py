bd604e9e0fe6b0fbda2b30a5d7138d369cadb4f2
ruslan <ruslan.13.sv@gmail.com>
2022-08-18 22:09:33 +0300

    sync

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,11 +1,12 @@
-from sqlalchemy import Column, String, Integer
+from sqlalchemy import Column, String, Integer, ForeignKey, Time, Table
 
 
 from ..db import Base
+from ..users.models import User
 
 
 class Tag(Base):
-    __tablename__ = 'Tag'
+    __tablename__ = 'tag'
 
     id = Column(Integer, primary_key=True)
     name = Column(String)
@@ -13,7 +14,7 @@ class Tag(Base):
 
 
 class Ingredient(Base):
-    __tablename__ = 'Ingredient'
+    __tablename__ = 'ingredient'
 
     id = Column(Integer, primary_key=True)
     name = Column(String)
@@ -21,7 +22,24 @@ class Ingredient(Base):
 
 
 class Recipe(Base):
-    __tablename__ = 'Recipe'
+    __tablename__ = 'recipe'
 
     id = Column(Integer, primary_key=True)
     name = Column(String)
+    owner = Column(Integer, ForeignKey(User.id), index=True)
+    text = Column(String)
+    # TODO: Разобраться с валидацией времени (не менее 1 минуты)
+    cooking_time = Column(Time)
+    tag = Column(Integer, ForeignKey('tag.id'))
+    # TODO: Разобраться как хранить картинки
+    image = Column()
+    ingredients = Column(Ingredient, ForeignKey('ingredient.id'))
+
+
+amount_table = Table(
+    'amount_table',
+    Base.metadata,
+    Column('recipe', ForeignKey('recipe.id')),
+    Column('ingredient', ForeignKey('ingredient.id')),
+    Column('amount', Integer)
+)
---
+++ backend/recipes/schemas.py
@@ -0,0 +1,9 @@
+from pydantic import BaseModel
+
+
+class Tag(BaseModel):
+    pass
+
+
+class Ingredient(BaseModel):
+    pass
